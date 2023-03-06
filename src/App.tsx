6df1db18190b0a832e0fcc119e6a4995f5d8bef6
stoshich <stoshich.stefan@yandex.ru>
2023-03-06 15:30:05 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="constructor-bar">
+
+      </div>
+      <div className="canvas">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
